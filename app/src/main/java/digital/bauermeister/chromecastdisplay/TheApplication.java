67932716f96634c5dc08bb21317e5eee8fb679e6
Pascal Bauermeister <pascal@privately.eu>
2016-03-13 09:18:42 +0100

    Belongs to last commit.

--- app/src/main/java/digital/bauermeister/chromecastdisplay/TheApplication.java
+++ app/src/main/java/digital/bauermeister/chromecastdisplay/TheApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Intent;
 import android.util.Log;
 
-import digital.bauermeister.chromecastdisplay.service_litvak.LitvakBasedService;
 import digital.bauermeister.chromecastdisplay.service_nodejs.NodejsBasedService;
 
 public class TheApplication extends Application {
@@ -16,7 +15,6 @@ public class TheApplication extends Application {
 
         Log.d(TAG, "*** APP onCreate ***");
 
-//        startService(new Intent(this, LitvakBasedService.class));
         startService(new Intent(this, NodejsBasedService.class));
 
         Log.d(TAG, "*** APP onCreate done");
